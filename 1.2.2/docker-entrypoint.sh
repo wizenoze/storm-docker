2519cab6d6013600d39432db6a3e50d8dcc3f531
Laszlo Csontos <github@laszlocsontos.com>
2018-10-11 11:49:08 +0200

    Enable profiling and Reconfigure JVM debugging options for OpenJDK 11

--- 1.2.2/docker-entrypoint.sh
+++ 1.2.2/docker-entrypoint.sh
@@ -17,6 +17,16 @@ storm.zookeeper.servers: [zookeeper]
 nimbus.seeds: [nimbus]
 storm.log.dir: "$STORM_LOG_DIR"
 storm.local.dir: "$STORM_DATA_DIR"
+
+worker.profiler.enabled: true
+
+# Leverage unified logging for debugging the GC, old options aren't valid any more
+# See: http://openjdk.java.net/jeps/271
+worker.childopts: "-Xmx%HEAP-MEM%m -Xlog:gc*=debug:file=artifacts/gc-%WORKER-ID%.log:utctime,uptime,tid,level:filecount=10,filesize=128m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=artifacts/heapdump-%WORKER-ID%.hprof"
+
+# Customize JFR options, -XX:+UnlockCommercialFeatures gives an error on OpenJDK 11
+# See: https://docs.oracle.com/en/java/javase/11/tools/java.html#GUID-3B1CE181-CD30-4178-9602-230B800D4FAE
+worker.profiler.childopts: "-XX:StartFlightRecording=disk=true,dumponexit=true,filename=artifacts/recording-%WORKER-ID%.jfr,maxsize=1024m,maxage=1d,settings=default"
 EOF
 fi
 
