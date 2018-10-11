c4b8b50faca5aa0fb8801e01ccd6149fc7ca5048
Laszlo Csontos <github@laszlocsontos.com>
2018-10-11 10:14:26 +0200

    Replace Alpine specific parts - openjdk:11 is based on Debian

--- 1.2.2/docker-entrypoint.sh
+++ 1.2.2/docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 # Allow the container to be started with `--user`
 if [ "$1" = 'storm' -a "$(id -u)" = '0' ]; then
     chown -R "$STORM_USER" "$STORM_CONF_DIR" "$STORM_DATA_DIR" "$STORM_LOG_DIR"
-    exec su-exec "$STORM_USER" "$0" "$@"
+    exec gosu "$STORM_USER" "$0" "$@"
 fi
 
 # Generate the config only if it doesn't exist
